5572fea515fbaa8d5e1448f2842f1d6a5cd0ecca
Fernanda Souza <fecafumee@gmail.com>
2023-08-31 13:09:16 +0200

    updated logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,10 @@
 import './Navbar.scss';
-import logo from '../../images/logo.png';
+// import logo from '../../images/logo.png';
 
 function Navbar() {
   return (
     <div className='navbar'>
-      <img src={logo} alt='logo of foodelicious' />
+      <img src='../../images/logo.png' alt='logo of foodelicious' />
       <h1>FooDelicious</h1>
     </div>
   );
